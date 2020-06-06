e9481539db3a1605b30e650edba06958ef1da0be
Arnold Rozon <arnold@rozon.org>
2020-06-06 02:29:30 -0500

    Routers and cleanup

---
+++ routes/studios.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const scraper = require("../utils/Scraper");
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await scraper.studios;
+    res.status(200).send({ success: true, data: response });
+  } catch (error) {
+    res.status(500).send({ error: true, message: error.message });
+  }
+});
+
+module.exports = router;
