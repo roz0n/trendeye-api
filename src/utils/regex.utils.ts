8fa5e6161cc821542acd2eb4c2173ecdd1f0a952
Arnold Rozon <arnold@rozon.org>
2021-04-18 17:27:17 -0500

    Typos inside regex utils

--- src/utils/regex.utils.ts
+++ src/utils/regex.utils.ts
@@ -1,6 +1,6 @@
 // Match all chars between parens including parens
 export const parensRegex = /\(([^)]+)\)/g;
-// Match all numertical chars
+// Match all numerical chars
 export const numsRegex = /[0-9]+/g;
-// Math all possible new line chars
+// Match all possible new line chars
 export const newLineRegex = /(?:\r\n|\r|\n)/g;
