6e11263a1328872d8694b88cb4397b56cee82e96
Arnold Rozon <arnold@rozon.org>
2021-08-04 21:40:42 -0500

    Working on refactoring scraping

--- src/scripts/scrapeImages.scripts.ts
+++ src/scripts/scrapeImages.scripts.ts
@@ -1,9 +1,11 @@
 import fs from "fs";
 // TODO: Remove got usage: https://stackoverflow.com/questions/60202993/how-to-direct-the-stream-of-an-audio-file-through-express-js-if-it-is-on-another
-// import axios from "axios";
-import got from "got";
+import axios from "axios";
+// import got from "got";
 import ImageRipper from "../services/scraper/imageRipper.service";
 import { pipeline } from "stream";
+
+const SLEEP_DURATION = 3000;
 
 export function sleep(ms: number) {
   return new Promise((resolve) => {
@@ -13,7 +15,7 @@ export function sleep(ms: number) {
 
 export async function scrapeImages(categoryNames: string[]) {
   console.log("Beginning image scraping...");
-  sleep(2000);
+  sleep(SLEEP_DURATION);
 
   let errors: Array<{
     category: string;
@@ -28,7 +30,7 @@ export async function scrapeImages(categoryNames: string[]) {
     console.log(`Obtaining image urls for category: ${category}`);
 
     let images = await ImageRipper.getImageUrls(category);
-    sleep(2000);
+    sleep(SLEEP_DURATION);
 
     console.log("Complete!");
     console.log(`Images for: ${category}:`, images);
@@ -38,25 +40,46 @@ export async function scrapeImages(categoryNames: string[]) {
       const filePath = `./data/${
         i % 2 === 0 ? "training" : "test"
       }/${category}/${images![i]!.name}.png`;
+      const url = images![i]!.url;
 
-      sleep(3000); // limit requests so we don't overwhelm the server and risk rate-limiting or something else, the site seems brittle
+      sleep(SLEEP_DURATION); // limit requests so we don't overwhelm the server and risk rate-limiting or something else, the site seems brittle
 
-      await pipeline(
-        got.stream(images![i]!.url),
-        fs.createWriteStream(filePath),
-        function (error) {
-          if (error) {
-            console.log("Error saving image:", error);
+      // await pipeline(
+      //   got.stream(images![i]!.url),
+      //   fs.createWriteStream(filePath),
+      //   function (error) {
+      //     if (error) {
+      //       console.log("Error saving image:", error);
+      //       console.log("Logging error...");
+      //       errors.push({
+      //         category: category,
+      //         index: i,
+      //         name: images![i]!.name,
+      //         url: images![i]!.url,
+      //       });
+      //       sleep(SLEEP_DURATION);
+      //     }
+      //     console.log(`Saved image ${i}/${images!.length}`);
+      //   }
+      // );
+
+      await axios({ method: "get", url, responseType: "stream" }).then(
+        (response) => {
+          if (response.status !== 200) {
+            console.log("Error saving image...");
             console.log("Logging error...");
+
             errors.push({
               category: category,
               index: i,
               name: images![i]!.name,
               url: images![i]!.url,
             });
-            sleep(2000);
+
+            sleep(SLEEP_DURATION);
+          } else {
+            fs.createWriteStream(filePath);
           }
-          console.log(`Saved image ${i}/${images!.length}`);
         }
       );
     }
